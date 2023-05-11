cea4f5791803fbdce3df69e8b95d4e5ab7a82978
Arjen Postma <info@postma-dev.nl>
2023-05-11 14:25:24 +0200

    Also expose src files when installing package

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,25 +1,25 @@
-import { defineComponent as s, openBlock as _, createElementBlock as r, toDisplayString as p } from "vue";
+import { defineComponent as c, openBlock as _, createElementBlock as r, toDisplayString as p } from "vue";
 class f {
   ping() {
     return console.info("ping!"), "PING!";
   }
 }
-const a = { class: "personal-button" }, u = /* @__PURE__ */ s({
+const a = { class: "personal-button" }, u = /* @__PURE__ */ c({
   __name: "etButton",
   props: {
     text: {}
   },
   setup(t) {
-    const e = t;
-    return (o, n) => (_(), r("button", a, p(e.text), 1));
+    const o = t;
+    return (e, n) => (_(), r("button", a, p(o.text), 1));
   }
 });
-const l = (t, e) => {
-  const o = t.__vccOpts || t;
-  for (const [n, c] of e)
-    o[n] = c;
-  return o;
-}, d = /* @__PURE__ */ l(u, [["__scopeId", "data-v-87c4ec5f"]]);
+const l = (t, o) => {
+  const e = t.__vccOpts || t;
+  for (const [n, s] of o)
+    e[n] = s;
+  return e;
+}, d = /* @__PURE__ */ l(u, [["__scopeId", "data-v-31987fc7"]]);
 export {
   d as EtButton,
   f as Sonar
