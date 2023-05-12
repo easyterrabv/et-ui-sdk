adb702ef3a62af4163c5b0dcfa01dab60d99b237
Arjen Postma <info@postma-dev.nl>
2023-05-12 10:51:16 +0200

    linting fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Models
 export * from "./models/sonar";
 
-import './components/tailwind.css';
+import "./components/tailwind.css";
 
 // Components
 import EtButton from "./components/etButton.vue";
