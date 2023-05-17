dc46644b886b43202699c0b698365a778a806a0f
Arjen Postma <info@postma-dev.nl>
2023-05-17 11:57:29 +0200

    Format

--- src/components/etIcons/EtIconHouse.vue
+++ src/components/etIcons/EtIconHouse.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 </script>
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ import "./tailwind.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
-library.add(
-    faHouse
-);
+library.add(faHouse);
 
 // Components
 import EtButton from "./components/etButton/EtButton.vue";
