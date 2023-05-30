faa85b2c25b9967220a1b7a628df1c76f0fcc073
Arjen Postma <info@postma-dev.nl>
2023-05-30 11:33:14 +0200

    Added form component

---
+++ src/components/etForm/EtForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form
+        :method="method"
+        :onsubmit="onsubmit"
+        @keyup.enter.stop="$emit('submit')"
+    >
+        <slot />
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({
+    props: {
+        method: {
+            type: String,
+            required: false,
+            default: "post"
+        },
+        onsubmit: {
+            type: Function,
+            required: false,
+            default() {
+                return () => false;
+            }
+        }
+    },
+    emits: {
+        submit: () => true
+    }
+});
+</script>
