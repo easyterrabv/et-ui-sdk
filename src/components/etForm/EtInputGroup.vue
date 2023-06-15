0a1b03d45d2971816e63beb78f28e0a6ecd6927e
Arjen Postma <info@postma-dev.nl>
2023-06-15 16:32:16 +0200

    EtInputGroup: Resolved an issue where inputs would be displayed incorrectly if they were in a nested EtInputGroup

--- src/components/etForm/EtInputGroup.vue
+++ src/components/etForm/EtInputGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex flex-row [&>.et-input-wrapper]:grow [&>*]:!rounded-none [&_.et-input]:!rounded-none [&_.et-input-like]:!rounded-none [&>*:first-child]:!rounded-l-md [&>*:last-child]:!rounded-r-md [&>*:first-child_.et-input]:!rounded-l-md [&>*:last-child_.et-input]:!rounded-r-md [&>*:first-child_.et-input-like]:!rounded-l-md [&>*:last-child_.et-input-like]:!rounded-r-md"
+        class="et-input-group flex flex-row [&>.et-input-wrapper]:grow [&>*]:!rounded-none [&_.et-input]:!rounded-none [&_.et-input-like]:!rounded-none [&>*:not(.et-input-group):first-child]:!rounded-l-md [&>*:not(.et-input-group):last-child]:!rounded-r-md [&>*:not(.et-input-group):first-child_.et-input]:!rounded-l-md [&>*:not(.et-input-group):last-child_.et-input]:!rounded-r-md [&>*:first-child_.et-input-like]:!rounded-l-md [&>*:last-child_.et-input-like]:!rounded-r-md"
     >
         <slot></slot>
     </div>
