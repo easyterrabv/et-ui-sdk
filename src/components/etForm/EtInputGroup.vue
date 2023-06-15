a616fe109789da274434ea1a2dc17cbf6a2e156d
Arjen Postma <info@postma-dev.nl>
2023-06-15 11:22:42 +0200

    EtInputGroup: Allow for "et-input-like" classes to be used in the group

--- src/components/etForm/EtInputGroup.vue
+++ src/components/etForm/EtInputGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex flex-row [&>.et-input-wrapper]:grow [&>*]:!rounded-none [&_.et-input]:!rounded-none [&>*:first-child]:!rounded-l-md [&>*:last-child]:!rounded-r-md [&>*:first-child_.et-input]:!rounded-l-md [&>*:last-child_.et-input]:!rounded-r-md"
+        class="flex flex-row [&>.et-input-wrapper]:grow [&>*]:!rounded-none [&_.et-input]:!rounded-none [&_.et-input-like]:!rounded-none [&>*:first-child]:!rounded-l-md [&>*:last-child]:!rounded-r-md [&>*:first-child_.et-input]:!rounded-l-md [&>*:last-child_.et-input]:!rounded-r-md [&>*:first-child_.et-input-like]:!rounded-l-md [&>*:last-child_.et-input-like]:!rounded-r-md"
     >
         <slot></slot>
     </div>
