b7c961ee6e76c67ff127f49ff888d60c7b066b15
Arjen Postma <info@postma-dev.nl>
2023-06-20 14:20:39 +0200

    Added ArrowDown and Spinner icons

---
+++ src/components/etIcon/EtIconArrowDown.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="arrow-down" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+library.add(faArrowDown);
+
+import EtIcon from "src/components/etIcon/EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconSpinner.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="spinner" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+library.add(faSpinner);
+
+import EtIcon from "src/components/etIcon/EtIcon.vue";
+</script>
