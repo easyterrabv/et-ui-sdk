a1b3a5e8f489f84469e2515783bd214ea7afa18f
Arjen Postma <info@postma-dev.nl>
2023-06-30 14:02:51 +0200

    EtDragScrollBox: Improvements

--- src/components/etUtilities/EtDragScrollBox.vue
+++ src/components/etUtilities/EtDragScrollBox.vue
@@ -17,28 +17,42 @@ import { defineComponent } from "vue-demi";
 export default defineComponent({
     data() {
         return {
+            isMouseDown: false,
             isDragging: false,
             startY: 0,
             scrollTop: 0
         };
     },
+    watch: {
+        isDragging() {
+            if (this.isDragging) {
+                this.$emit("dragStart");
+                return;
+            }
+            this.$emit("dragStop");
+        }
+    },
     methods: {
         startDrag(event) {
-            this.isDragging = true;
+            this.isMouseDown = true;
             this.startY = event.clientY;
             this.scrollTop = this.$refs.scrollContainer.scrollTop;
         },
         drag(event) {
-            if (!this.isDragging) return;
+            if (!this.isMouseDown) return;
+            this.isDragging = true;
             const deltaY = event.clientY - this.startY;
             this.scrollTo(this.scrollTop - deltaY);
         },
         stopDrag() {
+            this.isMouseDown = false;
             this.isDragging = false;
+            this.$emit("dragStop");
         },
         scrollTo(pos) {
             this.$refs.scrollContainer.scrollTop = pos;
         }
-    }
+    },
+    emits: ["dragStart", "dragStop"]
 });
 </script>
