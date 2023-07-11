f39cbded3b4fcb45687a489e8e288be693330c98
Arjen Postma <info@postma-dev.nl>
2023-07-11 15:13:48 +0200

    Added Pen Icon

---
+++ src/components/etIcon/EtIconPen.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="pen" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+library.add(faPen);
+
+import EtIcon from "./EtIcon.vue";
+</script>
