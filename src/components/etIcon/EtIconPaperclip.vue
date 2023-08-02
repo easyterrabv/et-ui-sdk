5892f626394dfe9d22a4ba65ebaa4ac3ede1f518
Arjen Postma <info@postma-dev.nl>
2023-08-02 11:16:35 +0200

    Added paperclip icon

---
+++ src/components/etIcon/EtIconPaperclip.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="paperclip" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+library.add(faPaperclip);
+
+import EtIcon from "./EtIcon.vue";
+</script>
