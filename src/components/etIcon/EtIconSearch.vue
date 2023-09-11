249048e69038b274c3b24d7034837314a3e6964e
Arjen Postma <info@postma-dev.nl>
2023-09-11 12:43:33 +0200

    Added EtIconSearch

---
+++ src/components/etIcon/EtIconSearch.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="magnifying-glass" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+library.add(faMagnifyingGlass);
+
+import EtIcon from "./EtIcon.vue";
+</script>
