f93d5ea9515a2c65bd3a6f7396728c70087aa6ab
Arjen Postma <info@postma-dev.nl>
2023-09-13 10:26:58 +0200

    Added EtIconSliders component

---
+++ src/components/etIcon/EtIconSliders.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="sliders" />
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+library.add(faSliders);
+
+import EtIcon from "./EtIcon.vue";
+</script>
