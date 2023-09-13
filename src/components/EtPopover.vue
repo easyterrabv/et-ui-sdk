44a176b937ff83db57c241f257ea180d05d04498
Arjen Postma <info@postma-dev.nl>
2023-09-13 10:22:18 +0200

    EtPopover: Fixed typescript errors/warnings

--- src/components/EtPopover.vue
+++ src/components/EtPopover.vue
@@ -48,9 +48,9 @@ export default defineComponent({
         return {
             hasFocus: false as Boolean,
             toggleWidth: 0 as Number,
-            buttonElement: null as HTMLElement,
-            tooltipElement: null as HTMLElement,
-            popperInstance: null as Instance
+            buttonElement: undefined as HTMLElement | undefined,
+            tooltipElement: undefined as HTMLElement | undefined,
+            popperInstance: undefined as Instance | undefined
         };
     },
     computed: {
@@ -64,37 +64,37 @@ export default defineComponent({
         }
     },
     methods: {
-        async togglePopover() {
+        async togglePopover(): Promise<void> {
             if (this.hasFocus) {
                 return await this.setPopoverFocus(false);
             }
 
-            return await this.setPopoverFocus(true);
+            await this.setPopoverFocus(true);
         },
-        async setPopoverFocus(focus) {
+        async setPopoverFocus(focus: boolean): Promise<void> {
             if (focus) {
                 await this.open();
-                this.buttonElement.focus();
+                this.buttonElement?.focus();
             } else {
                 await this.hide();
-                this.buttonElement.blur();
+                this.buttonElement?.blur();
             }
         },
         async open() {
             this.calculateToggleWidth();
             this.hasFocus = true;
             this.$emit("focus");
-            await this.popperInstance.update();
+            await this.popperInstance?.update();
         },
         async hide() {
             await wait(150);
             this.hasFocus = false;
             this.$emit("blur");
-            await this.popperInstance.update();
+            await this.popperInstance?.update();
         },
         calculateToggleWidth() {
-            const bounds = this.buttonElement.getBoundingClientRect();
-            this.toggleWidth = bounds.width;
+            const bounds = this.buttonElement?.getBoundingClientRect();
+            this.toggleWidth = bounds?.width || 300;
         },
         isOpen() {
             return this.hasFocus;
@@ -105,11 +105,11 @@ export default defineComponent({
         blur: () => true
     },
     mounted() {
-        this.buttonElement = this.$refs.toggle;
-        this.tooltipElement = this.$refs.content;
+        this.buttonElement = this.$refs.toggle as any;
+        this.tooltipElement = this.$refs.content as any;
         this.popperInstance = createPopper(
-            this.buttonElement,
-            this.tooltipElement,
+            this.buttonElement as any,
+            this.tooltipElement as any,
             {
                 placement: "bottom"
             }
