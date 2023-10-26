ab36f541305a356e203f72a218094c82a43a63f0
Arjen Postma <info@postma-dev.nl>
2023-10-26 14:35:08 +0200

    EtTopBar: Replace Tailwind with regular CSS

--- src/components/etMenu/EtTopBar.vue
+++ src/components/etMenu/EtTopBar.vue
@@ -1,12 +1,37 @@
 <template>
-    <nav class="w-full h-14 bg-slate-800 px-4 py-2 flex flex-row align-middle">
-        <div class="flex-grow flex place-items-center">
+    <nav class="et-sdk-top-bar-nav">
+        <div class="et-sdk-top-bar-nav--main">
             <slot></slot>
         </div>
-        <div class="flex place-items-center">
+        <div class="et-sdk-top-bar-nav--addon">
             <slot name="end"></slot>
         </div>
     </nav>
 </template>
 
 <script setup lang="ts"></script>
+
+<style>
+.et-sdk-top-bar-nav {
+    display: flex;
+    flex-direction: row;
+
+    width: 100%;
+    height: 66px;
+
+    padding: 15px 30px;
+
+    background-color: #1e293b;
+}
+
+.et-sdk-top-bar-nav--main {
+    display: flex;
+    flex-grow: 1;
+    place-items: center;
+}
+
+.et-sdk-top-bar-nav--addon {
+    display: flex;
+    place-items: center;
+}
+</style>
