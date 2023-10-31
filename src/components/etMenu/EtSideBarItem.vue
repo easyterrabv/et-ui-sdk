51a1c1e3c19bb43079c2188edb7c3eb23ea43356
Arjen Postma <info@postma-dev.nl>
2023-10-31 14:22:27 +0100

    EtSideBarItem: Changed tailwind styling to regular css

--- src/components/etMenu/EtSideBarItem.vue
+++ src/components/etMenu/EtSideBarItem.vue
@@ -1,22 +1,18 @@
 <template>
     <div
-        class="py-1 hover:bg-white cursor-pointer text-sm transition-all ease-in-out duration-150"
-        :class="[
-            {
-                'bg-primary hover:!bg-primary-dark text-white': isActive,
-                'mr-6 rounded-r-full pl-4': !isCollapsed,
-                'mx-2 rounded-full text-center': isCollapsed
-            }
-        ]"
+        class="et-sdk-sidebar-item"
+        :class="{
+            'et-sdk-sidebar-item__active': isActive,
+            'et-sdk-sidebar-item__sidebar-open': !isCollapsed,
+            'et-sdk-sidebar-item__sidebar-closed': isCollapsed
+        }"
     >
         <span
             v-if="$slots.icon"
-            class="w-3 px-2 text-primary"
-            :class="[
-                {
-                    '!text-white': isActive
-                }
-            ]"
+            class="et-sdk-sidebar-item--icon"
+            :class="{
+                'et-sdk-sidebar-item__active--icon': isActive
+            }"
         >
             <slot name="icon"></slot>
         </span>
@@ -44,3 +40,52 @@ export default defineComponent({
     }
 });
 </script>
+
+<style>
+.et-sdk-sidebar-item {
+    padding-top: 4px;
+    padding-bottom: 4px;
+    cursor: pointer;
+    font-size: var(--et-sdk-font-size-small);
+    transition: all 150ms ease-in-out;
+    background-color: var(--et-sdk-dark-50);
+}
+
+.et-sdk-sidebar-item__active {
+    color: var(--et-sdk-light-0);
+    background-color: var(--et-sdk-blue-600);
+}
+
+.et-sdk-sidebar-item__active:hover {
+    background-color: var(--et-sdk-blue-700);
+}
+
+.et-sdk-sidebar-item:hover {
+    background-color: white;
+}
+
+.et-sdk-sidebar-item__sidebar-open {
+    margin-right: 24px;
+    border-top-right-radius: 17px;
+    border-bottom-right-radius: 17px;
+    padding-left: 8px;
+}
+
+.et-sdk-sidebar-item__sidebar-closed {
+    margin-left: 8px;
+    margin-right: 8px;
+    border-radius: 99999px;
+    text-align: center;
+}
+
+.et-sdk-sidebar-item--icon {
+    width: 12px;
+    padding-left: 8px;
+    padding-right: 8px;
+    color: var(--et-sdk-blue-600);
+}
+
+.et-sdk-sidebar-item__active--icon {
+    color: white;
+}
+</style>
