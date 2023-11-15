2cdf58885eb062eae9dabad3c5bf69a4e719cced
Arjen Postma <info@postma-dev.nl>
2023-11-15 11:51:55 +0100

    Added sort icons

---
+++ src/components/etIcon/EtIconSortUp.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="sort-up" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSortUp } from "@fortawesome/free-solid-svg-icons";
+library.add(faSortUp);
+
+import EtIcon from "./EtIcon.vue";
+</script>
