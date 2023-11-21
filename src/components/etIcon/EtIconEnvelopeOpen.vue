410720fc4d0f90604d2aa84206eaf8899fe2da54
Arjen Postma <info@postma-dev.nl>
2023-11-21 12:12:21 +0100

    Added: EtIconEnvelopeOpen

---
+++ src/components/etIcon/EtIconEnvelopeOpen.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="envelope-open" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
+library.add(faEnvelopeOpen);
+
+import EtIcon from "./EtIcon.vue";
+</script>
