7ba07afdcd4ebfedf0174ff97f1e7d6779facbdc
Arjen Postma <info@postma-dev.nl>
2023-11-21 14:19:49 +0100

    Added: tooltip direction prop - default bottom

--- src/components/EtToolTip.vue
+++ src/components/EtToolTip.vue
@@ -1,11 +1,31 @@
 <template>
     <span class="et-sdk-tooltip-wrapper">
         <slot></slot>
-        <span class="et-sdk-tooltip-wrapper--tooltip no-wrap">
+        <span
+            class="et-sdk-tooltip-wrapper--tooltip no-wrap"
+            :class="{
+                'et-sdk-tooltip-wrapper--tooltip__bottom':
+                    props.direction === 'bottom',
+                'et-sdk-tooltip-wrapper--tooltip__right':
+                    props.direction === 'right'
+            }"
+        >
             <slot name="tooltip"></slot>
         </span>
     </span>
 </template>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+const props = defineProps({
+    direction: {
+        type: String as PropType<"bottom" | "right">,
+        required: false,
+        default: "bottom"
+    }
+});
+</script>
 
 <style>
 .et-sdk-tooltip-wrapper {
@@ -20,15 +40,11 @@
     display: none;
 
     position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translateX(-50%);
 
     background-color: var(--et-sdk-dark-600);
     color: var(--et-sdk-dark-50);
     border-radius: 99999px;
     line-height: initial;
-    margin-top: 6px;
     padding: 4px 16px;
     font-size: var(--et-sdk-font-size-tiny);
     box-shadow: var(--et-sdk-shadow-normal);
@@ -39,13 +55,36 @@
 .et-sdk-tooltip-wrapper--tooltip:after {
     content: "";
     position: absolute;
-    bottom: 100%;
-    left: 50%;
-    transform: translateX(-50%);
     width: 0;
     height: 0;
     border-width: 4px;
     border-style: solid;
     border-color: transparent transparent var(--et-sdk-dark-600) transparent;
+}
+
+.et-sdk-tooltip-wrapper--tooltip__bottom {
+    top: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: 6px;
+}
+
+.et-sdk-tooltip-wrapper--tooltip__bottom:after {
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+.et-sdk-tooltip-wrapper--tooltip__right {
+    top: 50%;
+    left: 100%;
+    transform: translateY(-50%);
+    margin-left: 6px;
+}
+
+.et-sdk-tooltip-wrapper--tooltip__right:after {
+    right: 100%;
+    transform: translate(1px, 2px);
+    border-color: transparent var(--et-sdk-dark-600) transparent transparent;
 }
 </style>
