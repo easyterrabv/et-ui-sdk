c235482172043ab8f803b4d579dbf5db47e7bf89
Arjen Postma <info@postma-dev.nl>
2023-11-21 12:06:48 +0100

    EtIcon: Added EtIconSquare

---
+++ src/components/etIcon/EtIconSquare.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="square" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSquare } from "@fortawesome/free-solid-svg-icons";
+library.add(faSquare);
+
+import EtIcon from "./EtIcon.vue";
+</script>
