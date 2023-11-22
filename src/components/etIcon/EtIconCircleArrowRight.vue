492995aa9206cd1e06910e34169ef7d9eefd2e02
Arjen Postma <info@postma-dev.nl>
2023-11-22 14:51:16 +0100

    Added two icons

---
+++ src/components/etIcon/EtIconCircleArrowRight.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle-arrow-right" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+library.add(faCircleArrowRight);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconEnvelopeRegular.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="envelope" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+library.add(faEnvelope);
+
+import EtIcon from "./EtIcon.vue";
+</script>
