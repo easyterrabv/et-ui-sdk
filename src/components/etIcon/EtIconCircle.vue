b6c8bab4d6b6488aed96ceef22632879abed9f66
Arjen Postma <info@postma-dev.nl>
2023-11-22 15:15:20 +0100

    EtIcon: Added circle icon

---
+++ src/components/etIcon/EtIconCircle.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+library.add(faCircle);
+
+import EtIcon from "./EtIcon.vue";
+</script>
