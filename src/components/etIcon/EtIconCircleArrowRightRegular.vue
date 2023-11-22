e3d32d89b4d5bb9bd0882e315f9a472d17364363
Arjen Postma <info@postma-dev.nl>
2023-11-22 15:23:19 +0100

    EtIcon: Added regular circle right icon

---
+++ src/components/etIcon/EtIconCircleArrowRightRegular.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="far" icon="circle-right" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleRight } from "@fortawesome/free-regular-svg-icons";
+library.add(faCircleRight);
+
+import EtIcon from "./EtIcon.vue";
+</script>
