e165fb1a9bbb073a5b9bebf63741054182d3af51
Arjen Postma <info@postma-dev.nl>
2023-12-04 10:43:38 +0100

    Changed disabled prop

--- src/components/etForm/EtCheckboxWithLabel.vue
+++ src/components/etForm/EtCheckboxWithLabel.vue
@@ -2,21 +2,29 @@
     <label
         class="et-sdk-checkbox-with-label"
         :class="{
-            'et-sdk-checkbox-with-label--disabled': disabled
+            'et-sdk-checkbox-with-label--disabled': props.disabled
         }"
         @click="handleOnClick"
     >
-        <EtCheckBox v-bind="$attrs" ref="checkbox" />
+        <EtCheckBox v-bind="$attrs" ref="checkbox" :disabled="props.disabled" />
         <slot />
     </label>
 </template>
 
 <script setup lang="ts">
 import EtCheckBox from "./EtCheckbox.vue";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 
 defineOptions({
     inheritAttrs: false
+});
+
+const props = defineProps({
+    disabled: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
 });
 
 const checkbox = ref<typeof EtCheckBox | null>(null);
@@ -28,17 +36,6 @@ function handleOnClick() {
 
     checkbox.value.handleOnClick();
 }
-
-const disabled = ref(false);
-watch(
-    // Because props fall through and boolean props can have empty string value in $attrs
-    // I decided to watch on the value inside the checkbox instead.
-    // const attrs = useAttrs();
-    // console.log(attrs.disabled); -> this would print empty string if <EtCheckboxWithLabel disabled />
-    () => checkbox.value?.disabled,
-    (value: boolean | undefined) => (disabled.value = !!value),
-    { immediate: true }
-);
 </script>
 
 <style>
