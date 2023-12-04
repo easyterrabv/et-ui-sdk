d1bee517f1cd3120fae3aadca9a60e9883cb6b19
Arjen Postma <info@postma-dev.nl>
2023-12-04 10:20:44 +0100

    Added EtCheckboxWithLabel component that uses EtCheckbox internally.

---
+++ src/components/etForm/EtCheckboxWithLabel.vue
@@ -0,0 +1,47 @@
+<template>
+    <label
+        class="et-sdk-checkbox-with-label"
+        :class="{
+            'et-sdk-checkbox-with-label--disabled': disabled
+        }"
+        @click="checkbox?.handleOnClick"
+    >
+        <EtCheckBox v-bind="$attrs" ref="checkbox" />
+        <slot></slot>
+    </label>
+</template>
+
+<script setup lang="ts">
+import EtCheckBox from "./EtCheckbox.vue";
+import { ref, watch } from "vue";
+
+defineOptions({
+    inheritAttrs: false
+});
+
+const disabled = ref(false);
+const checkbox = ref<typeof EtCheckBox | null>(null);
+watch(
+    // Because props fall through and boolean props can have empty string value in $attrs
+    // I decided to watch on the value inside the checkbox instead.
+    // const attrs = useAttrs();
+    // console.log(attrs.disabled); -> this would print empty string if <EtCheckboxWithLabel disabled />
+    () => checkbox.value?.disabled,
+    (value: boolean | undefined) => (disabled.value = !!value),
+    { immediate: true }
+);
+</script>
+
+<style>
+.et-sdk-checkbox-with-label {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    user-select: none;
+    cursor: pointer;
+}
+
+.et-sdk-checkbox-with-label--disabled {
+    cursor: not-allowed;
+}
+</style>
