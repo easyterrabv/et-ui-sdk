8039c299ea6f3eb0dc2de0407ef168fa13404a0a
Arjen Postma <info@postma-dev.nl>
2023-12-14 11:02:09 +0100

    Added stopwatch icon

---
+++ src/components/etIcon/EtIconStopwatch.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="stopwatch" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
+library.add(faStopwatch);
+
+import EtIcon from "./EtIcon.vue";
+</script>
