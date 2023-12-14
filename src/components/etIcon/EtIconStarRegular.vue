f2dbafb087348ddb5bf62de0e66dc19f495a2f8f
Arjen Postma <info@postma-dev.nl>
2023-12-14 11:01:39 +0100

    Added star regular icon

---
+++ src/components/etIcon/EtIconStarRegular.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="far" icon="star" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+library.add(faStar);
+
+import EtIcon from "./EtIcon.vue";
+</script>
