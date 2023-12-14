14933da02cb6dfe1da5b19b9f4682487f2824f94
Arjen Postma <info@postma-dev.nl>
2023-12-14 10:58:31 +0100

    Added inbox icon

---
+++ src/components/etIcon/EtIconInbox.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="inbox" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
+library.add(faInbox);
+
+import EtIcon from "./EtIcon.vue";
+</script>
