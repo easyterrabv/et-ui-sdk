f63a08ec08804740e2fd35d76fa35988c65c22b1
Arjen Postma <info@postma-dev.nl>
2023-12-20 08:00:22 +0100

    Added Circle Play Icon

---
+++ src/components/etIcon/EtIconCirclePlay.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle-play" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+library.add(faCirclePlay);
+
+import EtIcon from "./EtIcon.vue";
+</script>
