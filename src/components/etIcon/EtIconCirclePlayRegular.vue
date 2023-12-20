0a91a5a14a074cce90f0c8e1781b0b0e8f4c9aee
Arjen Postma <info@postma-dev.nl>
2023-12-20 08:23:12 +0100

    Added circle play regular and clock

---
+++ src/components/etIcon/EtIconCirclePlayRegular.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="far" icon="circle-play" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+library.add(faCirclePlay);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconClock.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="far" icon="clock" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+library.add(faClock);
+
+import EtIcon from "./EtIcon.vue";
+</script>
