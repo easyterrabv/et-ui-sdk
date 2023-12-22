0f6d020a3f9510656fe98f43d92a86d9c8c0fddc
Arjen Postma <info@postma-dev.nl>
2023-12-22 11:58:53 +0100

    Added Ellipsis Vertical icon

---
+++ src/components/etIcon/EtIconEllipsisVertical.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="ellipsis-vertical" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+library.add(faEllipsisVertical);
+
+import EtIcon from "./EtIcon.vue";
+</script>
