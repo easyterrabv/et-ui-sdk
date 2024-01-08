1d0ba78aa287b88f2f78b8e4f53591275f8dd6c6
Arjen Postma <info@postma-dev.nl>
2024-01-08 10:37:31 +0100

    Added headset Icon

---
+++ src/components/etIcon/EtIconHeadset.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="headset" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeadset } from "@fortawesome/free-solid-svg-icons";
+library.add(faHeadset);
+
+import EtIcon from "./EtIcon.vue";
+</script>
