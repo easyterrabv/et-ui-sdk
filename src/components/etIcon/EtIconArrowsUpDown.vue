c2b9390048feb43b3163815cc4508b5b9cdc60fe
Arjen Postma <info@postma-dev.nl>
2024-01-18 15:33:02 +0100

    Added: Arrows-up-down solid icon

---
+++ src/components/etIcon/EtIconArrowsUpDown.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="arrows-up-down" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowsUpDown } from "@fortawesome/free-solid-svg-icons";
+library.add(faArrowsUpDown);
+
+import EtIcon from "./EtIcon.vue";
+</script>
