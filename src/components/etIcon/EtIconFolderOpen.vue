768b254baa897c425b419c959ca58d5e1ef42d3d
Arjen Postma <info@postma-dev.nl>
2024-01-19 11:51:25 +0100

    Added folder open icon

---
+++ src/components/etIcon/EtIconFolderOpen.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="folder-open" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+library.add(faFolderOpen);
+
+import EtIcon from "./EtIcon.vue";
+</script>
