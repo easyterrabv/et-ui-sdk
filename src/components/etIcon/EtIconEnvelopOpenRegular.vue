5e063b3534daa4060a7ccae614e184a143a57d67
Arjen Postma <info@postma-dev.nl>
2024-01-19 11:53:23 +0100

    Added envelop open regular icon

---
+++ src/components/etIcon/EtIconEnvelopOpenRegular.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="far" icon="envelope-open" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
+library.add(faEnvelopeOpen);
+
+import EtIcon from "./EtIcon.vue";
+</script>
