39fa00063f00e8ccfc2964955b63a7c2dd90dfec
Arjen Postma <info@postma-dev.nl>
2024-01-23 16:34:56 +0100

    Added Icon: circle-exclamation

---
+++ src/components/etIcon/EtIconCircleExclamation.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle-exclamation" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+library.add(faCircleExclamation);
+
+import EtIcon from "./EtIcon.vue";
+</script>
