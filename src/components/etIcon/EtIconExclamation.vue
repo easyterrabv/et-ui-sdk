c4f9e03460e71d112f18c886e0a8b154e968f626
Arjen Postma <info@postma-dev.nl>
2024-01-23 16:39:58 +0100

    Added Icon: exclamation

---
+++ src/components/etIcon/EtIconExclamation.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="exclamation" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faExclamation } from "@fortawesome/free-solid-svg-icons";
+library.add(faExclamation);
+
+import EtIcon from "./EtIcon.vue";
+</script>
