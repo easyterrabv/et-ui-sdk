6ab055b4acaa3f47a4e7e8204708ec2046092cfe
Arjen Postma <info@postma-dev.nl>
2024-01-24 16:02:19 +0100

    Added: User Solid and User headset Solid

---
+++ src/components/etIcon/EtIconUserHeadsetSolid.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="fas" icon="user-headset" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserHeadset } from "@fortawesome/pro-solid-svg-icons";
+library.add(faUserHeadset);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconUserSolid.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="fas" icon="user" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/pro-solid-svg-icons";
+library.add(faUser);
+
+import EtIcon from "./EtIcon.vue";
+</script>
