10b26d748b485714b0f59a904e6817c34feedb8e
Arjen Postma <info@postma-dev.nl>
2024-02-08 11:00:19 +0100

    Added: Regular Alarm Clock Icon

---
+++ src/components/etIcon/EtIconAlarmClock.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="alarm-clock" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAlarmClock } from "@fortawesome/pro-regular-svg-icons";
+library.add(faAlarmClock);
+
+import EtIcon from "./EtIcon.vue";
+</script>
