d5dc55f1ca636007037bc2092a815b90882d9607
Arjen Postma <info@postma-dev.nl>
2024-02-21 16:15:45 +0100

    Added solid star

---
+++ src/components/etIcon/EtIconStarSolid.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="fas" icon="star" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/pro-solid-svg-icons";
+library.add(faStar);
+
+import EtIcon from "./EtIcon.vue";
+</script>
