3fb33fdd1fcc0b21e65c7a48edbabdb534f6fd3a
Arjen Postma <info@postma-dev.nl>
2024-02-22 12:14:33 +0100

    Added info circle icon

---
+++ src/components/etIcon/EtIconCircleInfo.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle-info" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleInfo } from "@fortawesome/pro-regular-svg-icons";
+library.add(faCircleInfo);
+
+import EtIcon from "./EtIcon.vue";
+</script>
