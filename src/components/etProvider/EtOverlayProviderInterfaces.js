10a3c1753eef82b1710ef1f0791b8648ed38bded
Arjen Postma <info@postma-dev.nl>
2024-03-11 11:35:24 +0100

    redid tsconfig. and deleted all js files

--- src/components/etProvider/EtOverlayProviderInterfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EtOverlayEvent = void 0;
-var EtOverlayEvent;
-(function (EtOverlayEvent) {
-    EtOverlayEvent[EtOverlayEvent["onClick"] = 0] = "onClick";
-    EtOverlayEvent[EtOverlayEvent["onShow"] = 1] = "onShow";
-    EtOverlayEvent[EtOverlayEvent["onHide"] = 2] = "onHide";
-})(EtOverlayEvent || (exports.EtOverlayEvent = EtOverlayEvent = {}));
--- src/models/Base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Base = void 0;
-var Base = /** @class */ (function () {
-    function Base() {
-    }
-    return Base;
-}());
-exports.Base = Base;
