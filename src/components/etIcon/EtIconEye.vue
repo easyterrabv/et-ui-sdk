680fb2af8a44c39a498eeb36ea2fea83b453fbe4
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:46:51 +0100

    Added eye icon

---
+++ src/components/etIcon/EtIconEye.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="eye" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEye } from "@fortawesome/pro-regular-svg-icons";
+library.add(faEye);
+
+import EtIcon from "./EtIcon.vue";
+</script>
