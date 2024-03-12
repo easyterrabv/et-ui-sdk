e6a4ebaa656ca247e0c1410d184dc09f4d2760c3
Arjen Postma <info@postma-dev.nl>
2024-03-12 12:48:58 +0100

    Added Icon Cogs and Icon Spellcheck

---
+++ src/components/etIcon/EtIconSpellCheck.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="spell-check" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSpellCheck } from "@fortawesome/pro-regular-svg-icons";
+library.add(faSpellCheck);
+
+import EtIcon from "./EtIcon.vue";
+</script>
