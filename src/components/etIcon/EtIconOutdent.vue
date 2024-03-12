8183f24602efd40276a7f17875124de7afcc9b50
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:50:05 +0100

    Added outdent icon

---
+++ src/components/etIcon/EtIconOutdent.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="outdent" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faOutdent } from "@fortawesome/pro-regular-svg-icons";
+library.add(faOutdent);
+
+import EtIcon from "./EtIcon.vue";
+</script>
