60c3c35e45ee1399e0d459730403b20b56b8ce72
Arjen Postma <info@postma-dev.nl>
2024-03-12 12:43:31 +0100

    Added chevron up icon

---
+++ src/components/etIcon/EtIconChevronUp.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="chevron-up" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronUp } from "@fortawesome/pro-regular-svg-icons";
+library.add(faChevronUp);
+
+import EtIcon from "./EtIcon.vue";
+</script>
