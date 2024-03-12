96f063adc07def7f1101007eeecf9a4162796c1a
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:23:11 +0100

    Added image icon

---
+++ src/components/etIcon/EtIconImage.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="image" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faImage } from "@fortawesome/pro-regular-svg-icons";
+library.add(faImage);
+
+import EtIcon from "./EtIcon.vue";
+</script>
