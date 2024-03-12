64129bc8c7ec6dcc938fe83bf83ad7df727de7c0
Arjen Postma <info@postma-dev.nl>
2024-03-12 12:12:11 +0100

    Added arrow left and arrow right icons

---
+++ src/components/etIcon/EtIconArrowLeft.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="arrow-left" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft } from "@fortawesome/pro-regular-svg-icons";
+library.add(faArrowLeft);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconArrowRight.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="arrow-right" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowRight } from "@fortawesome/pro-regular-svg-icons";
+library.add(faArrowRight);
+
+import EtIcon from "./EtIcon.vue";
+</script>
