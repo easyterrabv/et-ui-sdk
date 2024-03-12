99087d318174b126c53505f6edd23a21f2f39eba
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:27:43 +0100

    Added exchange alt icon

---
+++ src/components/etIcon/EtIconExchangeAlt.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="exchange-alt" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faExchangeAlt } from "@fortawesome/pro-regular-svg-icons";
+library.add(faExchangeAlt);
+
+import EtIcon from "./EtIcon.vue";
+</script>
