eae2a0330b95a8b02a7ca8bb7947fab710457e1b
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:40:51 +0100

    Added question icon

---
+++ src/components/etIcon/EtIconQuestion.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="question" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faQuestion } from "@fortawesome/pro-regular-svg-icons";
+library.add(faQuestion);
+
+import EtIcon from "./EtIcon.vue";
+</script>
