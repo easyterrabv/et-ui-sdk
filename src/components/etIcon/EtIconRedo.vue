62b99f12f7b6110d9306ef8d6b73a3812c1aa22f
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:43:04 +0100

    Added redo icon

---
+++ src/components/etIcon/EtIconRedo.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="redo" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faRedo } from "@fortawesome/pro-regular-svg-icons";
+library.add(faRedo);
+
+import EtIcon from "./EtIcon.vue";
+</script>
