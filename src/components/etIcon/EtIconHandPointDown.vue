0c9219fdd1e6c145ad3ad7767102ca8adc24145f
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:26:09 +0100

    Added hand point down icon

---
+++ src/components/etIcon/EtIconHandPointDown.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="hand-point-down" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHandPointDown } from "@fortawesome/pro-regular-svg-icons";
+library.add(faHandPointDown);
+
+import EtIcon from "./EtIcon.vue";
+</script>
