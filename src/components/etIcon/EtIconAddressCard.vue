36ec70e463bda46aac020c7314538e5b9d24ca53
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:48:52 +0100

    Added address card icon

---
+++ src/components/etIcon/EtIconAddressCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="address-card" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAddressCard } from "@fortawesome/pro-regular-svg-icons";
+library.add(faAddressCard);
+
+import EtIcon from "./EtIcon.vue";
+</script>
