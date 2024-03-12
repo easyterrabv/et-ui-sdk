78dfac1c30ee23b185155f881f083e0af95159c3
Arjen Postma <info@postma-dev.nl>
2024-03-12 15:59:38 +0100

    Added map marked alt icon

---
+++ src/components/etIcon/EtIconMapMarkedAlt.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="map-marked-alt" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMapMarkedAlt } from "@fortawesome/pro-regular-svg-icons";
+library.add(faMapMarkedAlt);
+
+import EtIcon from "./EtIcon.vue";
+</script>
