fa69478f9a626745ff4de7c5401129eaee94b771
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:24:49 +0100

    Added heading icon

---
+++ src/components/etIcon/EtIconHeading.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="heading" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeading } from "@fortawesome/pro-regular-svg-icons";
+library.add(faHeading);
+
+import EtIcon from "./EtIcon.vue";
+</script>
