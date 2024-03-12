fc7c2243b92e3bf19cc071fd515f7795c6de729b
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:40:15 +0100

    Added file icon

---
+++ src/components/etIcon/EtIconFile.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="file" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFile } from "@fortawesome/pro-regular-svg-icons";
+library.add(faFile);
+
+import EtIcon from "./EtIcon.vue";
+</script>
