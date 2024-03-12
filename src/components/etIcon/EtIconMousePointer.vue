ecd7e4fe0099b3b52b76db8f3944790164313a56
Arjen Postma <info@postma-dev.nl>
2024-03-12 13:44:48 +0100

    Added mouse pointer icon

---
+++ src/components/etIcon/EtIconMousePointer.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="mouse-pointer" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMousePointer } from "@fortawesome/pro-regular-svg-icons";
+library.add(faMousePointer);
+
+import EtIcon from "./EtIcon.vue";
+</script>
