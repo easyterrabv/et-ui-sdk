503628481899ab43c0a852db8f180b1d99ddb4ac
Arjen Postma <info@postma-dev.nl>
2024-03-13 11:23:25 +0100

    Added 1 icon for admin

---
+++ src/components/etIcon/EtIconSync.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="sync" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSync } from "@fortawesome/pro-regular-svg-icons";
+library.add(faSync);
+
+import EtIcon from "./EtIcon.vue";
+</script>
