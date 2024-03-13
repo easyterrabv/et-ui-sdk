284500f30c4256bf02016e22f5e926fe45217071
Arjen Postma <info@postma-dev.nl>
2024-03-13 11:37:15 +0100

    Added 1 icon for admin

---
+++ src/components/etIcon/EtIconCopy.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="copy" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCopy } from "@fortawesome/pro-regular-svg-icons";
+library.add(faCopy);
+
+import EtIcon from "./EtIcon.vue";
+</script>
