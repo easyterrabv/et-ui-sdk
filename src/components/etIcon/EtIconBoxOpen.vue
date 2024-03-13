713fe6bb5dbee69f7797191f7a6fbb996e98cf20
Arjen Postma <info@postma-dev.nl>
2024-03-13 10:46:32 +0100

    Added 2 more icons for admin

---
+++ src/components/etIcon/EtIconBoxOpen.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="box-open" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBoxOpen } from "@fortawesome/pro-regular-svg-icons";
+library.add(faBoxOpen);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconHandshake.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="handshake" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHandshake } from "@fortawesome/pro-regular-svg-icons";
+library.add(faHandshake);
+
+import EtIcon from "./EtIcon.vue";
+</script>
