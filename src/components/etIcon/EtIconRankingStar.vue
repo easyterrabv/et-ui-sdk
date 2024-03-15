e7015c255fb853018a855b49b70d942a07c62a59
Arjen Postma <info@postma-dev.nl>
2024-03-15 16:09:05 +0100

    Added Ranking Star Icon

---
+++ src/components/etIcon/EtIconRankingStar.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="ranking-star" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faRankingStar } from "@fortawesome/pro-regular-svg-icons";
+library.add(faRankingStar);
+
+import EtIcon from "./EtIcon.vue";
+</script>
