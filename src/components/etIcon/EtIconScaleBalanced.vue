ffb9486d3ded0f52fb63e12c9c83220d9dd2d68c
Arjen Postma <info@postma-dev.nl>
2024-03-15 15:04:28 +0100

    Added 1 icon for admin

---
+++ src/components/etIcon/EtIconScaleBalanced.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="scale-balanced" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faScaleBalanced } from "@fortawesome/pro-regular-svg-icons";
+library.add(faScaleBalanced);
+
+import EtIcon from "./EtIcon.vue";
+</script>
