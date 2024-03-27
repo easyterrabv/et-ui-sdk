b60f022aaf6c22babe84902247aa8d8a34947028
Arjen Postma <info@postma-dev.nl>
2024-03-27 15:17:30 +0100

    Added square check and square minus icons

---
+++ src/components/etIcon/EtIconSquareCheck.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="square-check" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSquareCheck } from "@fortawesome/pro-regular-svg-icons";
+library.add(faSquareCheck);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconSquareMinus.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="square-minus" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSquareMinus } from "@fortawesome/pro-regular-svg-icons";
+library.add(faSquareMinus);
+
+import EtIcon from "./EtIcon.vue";
+</script>
