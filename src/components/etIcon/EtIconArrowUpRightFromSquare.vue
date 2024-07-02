7742d719f312d132138e7257150efb5224fba1d8
Arjen Postma <info@postma-dev.nl>
2024-07-02 12:24:31 +0200

    Added: Arrow Up Right From Square Icon

---
+++ src/components/etIcon/EtIconArrowUpRightFromSquare.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="arrow-up-right-from-square" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowUpRightFromSquare } from "@fortawesome/pro-regular-svg-icons";
+library.add(faArrowUpRightFromSquare);
+
+import EtIcon from "./EtIcon.vue";
+</script>
