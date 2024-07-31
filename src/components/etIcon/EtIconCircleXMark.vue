c4acfc0e1a16b4b93135d8601697263ada8d8c05
Arjen Postma <info@postma-dev.nl>
2024-07-31 15:32:30 +0200

    Added two new icons

---
+++ src/components/etIcon/EtIconCircleXMark.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="circle-xmark" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleXmark } from "@fortawesome/pro-regular-svg-icons";
+library.add(faCircleXmark);
+
+import EtIcon from "./EtIcon.vue";
+</script>
