b144ce34a1ee4fab3c1be7cfa3550876877fb8a1
Arjen Postma <info@postma-dev.nl>
2024-09-11 11:43:49 +0200

    Added File Import icon

---
+++ src/components/etIcon/EtIconFileImport.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="file-import" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFileImport } from "@fortawesome/pro-regular-svg-icons";
+library.add(faFileImport);
+
+import EtIcon from "./EtIcon.vue";
+</script>
