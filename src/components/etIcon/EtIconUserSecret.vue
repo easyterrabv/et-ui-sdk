d822a2ecaaddf736c67cbc9d6fe033fdbf0f8c25
Arjen Postma <info@postma-dev.nl>
2024-09-19 10:59:03 +0200

    Added user secret regular icon

---
+++ src/components/etIcon/EtIconUserSecret.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="user-secret" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/pro-regular-svg-icons";
+library.add(faUserSecret);
+
+import EtIcon from "./EtIcon.vue";
+</script>
