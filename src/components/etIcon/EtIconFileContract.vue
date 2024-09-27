f8f0894db1c2b39966585b1f9a7c5dc035b5c9a7
Arjen Postma <info@postma-dev.nl>
2024-09-27 14:28:22 +0200

    Added EtIconFileContract and EtIconGlobePointer icons

---
+++ src/components/etIcon/EtIconFileContract.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="file-contract" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFileContract } from "@fortawesome/pro-regular-svg-icons";
+library.add(faFileContract);
+
+import EtIcon from "./EtIcon.vue";
+</script>
---
+++ src/components/etIcon/EtIconGlobePointer.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="globe-pointer" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGlobePointer } from "@fortawesome/pro-regular-svg-icons";
+library.add(faGlobePointer);
+
+import EtIcon from "./EtIcon.vue";
+</script>
