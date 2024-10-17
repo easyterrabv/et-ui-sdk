06f49cc88ffd41b2003184ad2b0960cfec55b96f
Arjen Postma <info@postma-dev.nl>
2024-10-17 15:16:23 +0200

    Added merge icon

---
+++ src/components/etIcon/EtIconMerge.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="merge" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMerge } from "@fortawesome/pro-regular-svg-icons";
+library.add(faMerge);
+
+import EtIcon from "./EtIcon.vue";
+</script>
