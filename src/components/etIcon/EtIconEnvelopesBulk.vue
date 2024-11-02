c026d61badc09543343aaf6c118328578bc67cf2
Arjen Postma <info@postma-dev.nl>
2024-11-02 07:30:55 +0100

    Added envelopes bulk icon

---
+++ src/components/etIcon/EtIconEnvelopesBulk.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="envelopes-bulk" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelopesBulk } from "@fortawesome/pro-regular-svg-icons";
+library.add(faEnvelopesBulk);
+
+import EtIcon from "./EtIcon.vue";
+</script>
