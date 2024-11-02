2ad11ce029a2422b85d87d437f37f037e74e28e9
Arjen Postma <info@postma-dev.nl>
2024-11-02 07:33:02 +0100

    Added envelopes bulk solid icon

---
+++ src/components/etIcon/EtIconEnvelopesSolid.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="fas" icon="envelopes-bulk" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelopesBulk } from "@fortawesome/pro-solid-svg-icons";
+library.add(faEnvelopesBulk);
+
+import EtIcon from "./EtIcon.vue";
+</script>
