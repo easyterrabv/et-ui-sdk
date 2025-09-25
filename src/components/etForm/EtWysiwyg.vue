5d1404c373b3b12ed292082d8f9054ea27066627
Arjen Postma <info@postma-dev.nl>
2025-09-25 15:13:35 +0200

    Removed unused import

--- src/components/etForm/EtWysiwyg.vue
+++ src/components/etForm/EtWysiwyg.vue
@@ -477,7 +477,6 @@ import { Table } from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
-import { Link } from "@tiptap/extension-link";
 import ImageResize from "tiptap-extension-resize-image";
 
 import EtButtonGroup from "../etButton/EtButtonGroup.vue";
