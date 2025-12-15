457702da8e3c0ec2916fe39969a9f380c30c9eaf
Arjen Postma <info@postma-dev.nl>
2025-12-15 14:44:43 +0100

    Add EtIconUserMinus component

---
+++ src/components/etIcon/EtIconUserMinus.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="user-minus" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserMinus } from "@fortawesome/pro-regular-svg-icons";
+library.add(faUserMinus);
+
+import EtIcon from "./EtIcon.vue";
+</script>
