0143899f3f47d575fcd80a4d7f184c5c8b08c2ec
Arjen Postma <info@postma-dev.nl>
2026-02-17 15:11:29 +0100

    Icon medal

---
+++ src/components/etIcon/EtIconMedal.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="medal" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMedal } from "@fortawesome/pro-regular-svg-icons";
+library.add(faMedal);
+
+import EtIcon from "./EtIcon.vue";
+</script>
