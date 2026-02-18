432e3bf18b0f8f427b5d0b80f4a2831c08cb61c2
Arjen Postma <info@postma-dev.nl>
2026-02-18 16:52:09 +0100

    Renamed EtIconDiamondExclamation to EtIconDiamondExclamationSolid

---
+++ src/components/etIcon/EtIconDiamondExclamationSolid.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon prefix="fas" icon="diamond-exclamation" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDiamondExclamation } from "@fortawesome/pro-solid-svg-icons";
+library.add(faDiamondExclamation);
+
+import EtIcon from "./EtIcon.vue";
+</script>
