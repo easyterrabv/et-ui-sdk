8db84acda5c2d87898a9da42376e07e97e0ffb8a
Arjen Postma <info@postma-dev.nl>
2026-03-13 14:45:38 +0100

    Added new EtIconDiamondExclamation component

---
+++ src/components/etIcon/EtIconDiamondExclamation.vue
@@ -0,0 +1,11 @@
+<template>
+    <EtIcon icon="diamond-exclamation" />
+</template>
+
+<script setup lang="ts">
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDiamondExclamation } from "@fortawesome/pro-regular-svg-icons";
+library.add(faDiamondExclamation);
+
+import EtIcon from "./EtIcon.vue";
+</script>
